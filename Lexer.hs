c32b0a47e5bad8b0e150f6ea18873a9cdbf404e8
Chris K <kottkech17@grinnell.edu>
2016-11-15 21:24:45 -0600

    Added rudimentary boolean operations. Numeric boolen operations do not work if the numbers are not surrounded by parentheses for some reason

--- Lexer.hs
+++ Lexer.hs
@@ -10,7 +10,7 @@ paren = do
   whitespace
   void $ string "you gotta"
   whitespace
-  e <- pexp
+  e <- expr
   whitespace
   void $ string "Morty"
   return (EParens e)
@@ -23,7 +23,7 @@ pmul = do
   whitespace
   string "times"
   whitespace
-  e2 <- pexp'
+  e2 <- opa'
   whitespace
   return $ EBin Mul e1 e2
 
@@ -33,37 +33,80 @@ pdiv = do
   whitespace
   string "divided by"
   whitespace
-  e2 <- pexp'
+  e2 <- opa'
   whitespace
   return $ EBin Div e1 e2
 
 padd = do
   whitespace
-  e1 <- pexp'
+  e1 <- opa'
   whitespace
   string "plus"
   whitespace
-  e2 <- pexp
+  e2 <- opa
   whitespace
   return $ EBin Add e1 e2
 
 psub = do
   whitespace
-  e1 <- pexp'
+  e1 <- opa'
   whitespace
   string "minus"
   whitespace
-  e2 <- pexp
+  e2 <- opa
   whitespace
   return $ EBin Sub e1 e2
 
-pexp = try padd <|> try psub <|> pexp'
+--BOOLEAN OPERATOR PARSER--
 
-pexp' = try pmul <|> try pdiv <|> pterm
+band = do
+  whitespace
+  e1 <- opb'
+  whitespace
+  string "and"
+  whitespace
+  e2 <- opb
+  whitespace
+  return $ EBin And e1 e2
 
---pexp  -> pexp'  + pexp | pexp'  - pexp | pexp'
---pexp' -> pterm  * pexp' | pterm `div` pexp' | pterm
---pterm -> base | (pexp)
+bor = do
+  whitespace
+  e1 <- opb'
+  whitespace
+  string "or"
+  whitespace
+  e2 <- opb
+  whitespace
+  return $ EBin Or e1 e2
+
+numEq = do
+  whitespace
+  e1 <- opa
+  whitespace
+  string "is the same as"
+  e2 <- opa
+  whitespace
+  return $ EBin Equals e1 e2
+
+--GRAMMAR--
+
+--expr -> opa | opb
+
+--opa  -> opa' + opa | opa'  - opa | opa'
+--opa' -> pterm  * opa' | pterm `div` opa' | pterm
+--pterm -> base | (opa)
+
+--opb  -> opb' = opb | opb' and opb | opb' or opb | opb'
+--opb' -> opa = opa | opa < opa | opa > opa | bterm
+--bterm -> True | False | (opb)
+
+expr = try opb <|> try opa
+
+--NUMBER OPS--
+
+opa = try padd <|> try psub <|> opa'
+
+opa' = try pmul <|> try pdiv <|> pterm
 
 pterm = try base <|> paren  --term
   where base = pint
@@ -71,7 +114,33 @@ pterm = try base <|> paren  --term
 pint :: Parser Exp
 pint = do
   n <- many1 digit
+  whitespace
   return $ EIntLit (read n)
 
+--BOOL OPS--
+
+opb  = try band {-<|> try beq-} <|> try bor <|> opb'
+opb' = try numEq <|> {-try numLt <|> try numGt <|>-} bterm
+bterm = try base <|> paren
+  where base = try bRight <|> try bWrong
+
+bRight :: Parser Exp
+bRight = do
+  whitespace
+  string "right"
+  return $ EBoolLit True
+
+bWrong :: Parser Exp
+bWrong = do
+  whitespace
+  string "wrong"
+  return $ EBoolLit False
+
 parseExp :: String -> Either ParseError Exp
-parseExp src = parse (pexp <* eof) "" src
+parseExp src = parse (expr <* eof) "" src
+
+
+--pbool -> int comp int | bool op pbool | bool
+--cond  -> "if" pbool "then" opa "else" opa
+
+
