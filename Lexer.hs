a373889e6a6d666c038c195110fc2655c0583782
Tristan Knoth <tjknoth@gmail.com>
2017-09-22 19:06:21 -0700

    starting Lexer...

--- Lexer.hs
+++ Lexer.hs
@@ -347,11 +347,44 @@ testParseExp :: String -> Either ParseError Exp
 testParseExp src = parse (expr <* eof) "" src
 -}
 
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlsh stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString s = return . 
+
+evalAndPrint :: String -> IO ()
+evalAndPrint s = evalString s >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+    if pred result
+      then return ()
+       else action result >> until_ pred prompt action
+
 --RUN FUNCTION: takes an input file, checks file extension, parses, and runs stepUni on the output parse tree
+--  If no arguments, starts REPL
 main :: IO ()
 main = do
-  filenames <- getArgs
-  grmFiles <- return $ filter (\f -> (takeExtension f) == ".grm") filenames
+  args <- getArgs
+  case length args of
+    0    -> runREPL
+    else -> compile args
+
+runREPL :: IO ()
+runREPL = do
+  putStrLn "Simulated parallelism features will not work with the REPL:"
+  until_ (== 'quit') (readPrompt "GRAMPA>>> ") evalAndPrint
+
+
+compile :: [String] -> IO ()
+compile fs = do  
+  grmFiles <- return $ filter (\f -> (takeExtension f) == ".grm") fs
   let file = case grmFiles of
                (x:xs) -> x
                []    -> error $ "Please pass valid GRAMPA file"
